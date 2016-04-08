1446c8c7563d9201439f33c8c2d58c470d1409ec
alexSpinola <alex.spinola.ss@gmail.com>
2016-04-08 17:08:28 +0100

    paso 1 y 2

---
+++ src/ut5pruebasintesisalexspinola/calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5pruebasintesisalexspinola;
+
+/**
+ *
+ * @author alexander
+ */
+public class calculadora {
+    private int operador1;
+    private int operador2;
+    private int operacion;
+}
