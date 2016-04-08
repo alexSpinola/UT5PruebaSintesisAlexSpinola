067f4b948878db36a3674d64bf19377bee45c52d
alexSpinola <alex.spinola.ss@gmail.com>
2016-04-08 17:12:38 +0100

    paso 3 y 4

--- src/ut5pruebasintesisalexspinola/calculadora.java
+++ src/ut5pruebasintesisalexspinola/calculadora.java
@@ -10,6 +10,30 @@ package ut5pruebasintesisalexspinola;
  * @author alexander
  */
 public class calculadora {
+
+    public int getOperador1() {
+        return operador1;
+    }
+
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    public int getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    public int getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(int operacion) {
+        this.operacion = operacion;
+    }
     private int operador1;
     private int operador2;
     private int operacion;
